0ed248828f0dad2c831bbdef9629989de1667090
javier <isbcmail@yahoo.es>
2022-05-22 14:27:11 +0200

    segunda modificacion

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
-public class HelloWorld {
+public class MyFirstJavaProgram {
 
-	public static void main(String[] args) {		
-		System.out.println("Hola Mundo");
-	}
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
 
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+   }
 }
